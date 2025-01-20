623a3ae0675fb47809335e4656cff99db4a9ce1a
Ustym Bordun <enderqwer12@gmail.com>
2025-01-20 19:00:39 +0200

    add timer and snackbar

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,82 @@
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+// import "izitoast/dist/css/iziToast.min.css";
+
+const snackbar = {
+  formEl: document.querySelector('.form'),
+  delay: null,
+  chosenState: null,
+
+  createPromise(chosenState, delay) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (chosenState === 'fulfilled') {
+          resolve(delay);
+        } else {
+          reject(delay);
+        }
+      }, delay);
+    });
+  },
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.delay = this.formEl.delay.value;
+    this.chosenState = this.formEl.state.value;
+
+    const { delay, chosenState } = this;
+
+    this.createPromise(chosenState, delay)
+      .then(delay => {
+        iziToast.success({
+          message: `Fulfilled promise in ${delay}ms`,
+          messageSize: '16',
+          messageColor: '#fff',
+          backgroundColor: '#59a10d',
+          position: 'topRight',
+          closeOnClick: true,
+          timeout: 3500,
+          close: false,
+          iconUrl: '../img/izi-toast-svg/success-icon.svg',
+        });
+
+        // console.log(chosenState);
+        // console.log(delay);
+      })
+      .catch(delay => {
+        iziToast.error({
+          message: `Rejected promise in ${delay}ms`,
+          messageSize: '16',
+          messageColor: '#fff',
+          backgroundColor: '#ef4040',
+          position: 'topRight',
+          closeOnClick: true,
+          timeout: 3500,
+          close: false,
+          iconUrl: '../img/izi-toast-svg/error-icon.svg',
+        });
+
+        // console.log(chosenState);
+        // console.log(delay);
+      });
+
+    this.formEl.reset();
+  },
+};
+
+snackbar.formEl.addEventListener(
+  'submit',
+  snackbar.onFormSubmit.bind(snackbar)
+);
+
+document.querySelectorAll('.form fieldset input').forEach(input => {
+  input.addEventListener('focus', event => {
+    event.target.closest('fieldset').classList.add('focused');
+  });
+
+  input.addEventListener('blur', event => {
+    event.target.closest('fieldset').classList.remove('focused');
+  });
+});
